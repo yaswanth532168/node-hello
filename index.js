11d04d639bfb8dd3a99ff0636063066d06981a24
EC2 Default User <ec2-user@ip-172-31-36-145.eu-north-1.compute.internal>
2026-02-03 12:54:20 +0000

    index file changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Yaswanth you task is completed!\n'
+  const msg = 'Hello world today yaswanth day\n'
 	
   res.end(msg);
 });
