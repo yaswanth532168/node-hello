eb27c26dba6a169d1b24a1eb7c235217e01b1b0d
EC2 Default User <ec2-user@ip-172-31-36-145.eu-north-1.compute.internal>
2026-02-04 08:31:17 +0000

    codes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-#const http = require('http');
-#const port = process.env.PORT || 3000;
+const http = require('http');
+const port = process.env.PORT || 3000;
 
-#const server = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello welcome to Bangalore \n'
 	
